7c9448444886ca824e46b935947bdc0cb32928f7
RatoX <rodrigovieira18@gmail.com>
2014-09-07 14:25:26 -0300

    Underscore adicionado

---
+++ app/scripts/services/util/underscore.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc factory
+ * @name cmsApp.util.underscore
+ * @description
+ * # oauth
+ * Factory in the cmsApp.
+ */
+angular.module('cmsApp')
+  .factory('_', function () {
+    return window._;
+  });
