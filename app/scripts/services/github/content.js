15a32b1718dd904e1eac8c8f7f8f56e053f00e78
RatoX <rodrigovieira18@gmail.com>
2014-09-08 14:33:48 -0300

    Dummy Commit

--- app/scripts/services/github/content.js
+++ app/scripts/services/github/content.js
@@ -13,7 +13,49 @@ angular.module('cmsApp')
       posts: function () {
         return [
           {
-           sha: '#3456',
+           sha: '#1',
+           name: '2014-10-10-opa.md',
+           year: 2014,
+           month: 10
+          },
+          {
+           sha: '#2',
+           name: '2014-10-10-opa.md',
+           year: 2014,
+           month: 10
+          },
+          {
+           sha: '#3',
+           name: '2014-10-10-opa.md',
+           year: 2014,
+           month: 10
+          },
+          {
+           sha: '#4',
+           name: '2014-10-10-opa.md',
+           year: 2014,
+           month: 10
+          },
+          {
+           sha: '#5',
+           name: '2014-10-10-opa.md',
+           year: 2014,
+           month: 10
+          },
+          {
+           sha: '#6',
+           name: '2014-10-10-opa.md',
+           year: 2014,
+           month: 10
+          },
+          {
+           sha: '#7',
+           name: '2014-10-10-opa.md',
+           year: 2014,
+           month: 10
+          },
+          {
+           sha: '#8',
            name: '2014-10-10-opa.md',
            year: 2014,
            month: 10
