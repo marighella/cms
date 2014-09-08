19528eaf67950eeaea2fda037cff452b7792e09f
RatoX <rodrigovieira18@gmail.com>
2014-09-08 15:06:01 -0300

    Dummy Commit

--- app/scripts/services/github/content.js
+++ app/scripts/services/github/content.js
@@ -14,49 +14,49 @@ angular.module('cmsApp')
         return [
           {
            sha: '#1',
-           name: '2014-10-10-opa.md',
+           name: 'Na Amazônia, Sem Terra enfrentam o grupo de Dantas',
            year: 2014,
            month: 10
           },
           {
            sha: '#2',
-           name: '2014-10-10-opa.md',
+           name: 'Ocupação em Goiás é batizada com o nome de Dom Tomás Balduíno',
            year: 2014,
            month: 10
           },
           {
            sha: '#3',
-           name: '2014-10-10-opa.md',
+           name: 'Onde está a Reforma Agrária no debate eleitoral?',
            year: 2014,
            month: 10
           },
           {
            sha: '#4',
-           name: '2014-10-10-opa.md',
+           name: 'JBS expõe trabalhadores ao frio e sofre a quarta condenação este ano',
            year: 2014,
            month: 10
           },
           {
            sha: '#5',
-           name: '2014-10-10-opa.md',
+           name: 'Três mil famílias do MST ocupam a fazenda Santa Mônica, do senador Eunício Oliveira',
            year: 2014,
            month: 10
           },
           {
            sha: '#6',
-           name: '2014-10-10-opa.md',
+           name: 'As grandes questões ausentes no debate eleitoral',
            year: 2014,
            month: 10
           },
           {
            sha: '#7',
-           name: '2014-10-10-opa.md',
+           name: 'Organizações fazem ajustes finais para plebiscito da reforma política',
            year: 2014,
            month: 10
           },
           {
            sha: '#8',
-           name: '2014-10-10-opa.md',
+           name: 'Dez empresas são donas de 73% das sementes de todo o mundo',
            year: 2014,
            month: 10
           },
