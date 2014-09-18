827c7e7993ff0c2180972eff5d8a984d66b09213
RatoX <rodrigovieira18@gmail.com>
2014-09-17 23:58:50 -0300

    refatorado load do ckeditor para melhor performace

--- app/scripts/directives/ck-editor.js
+++ app/scripts/directives/ck-editor.js
@@ -1,36 +1,132 @@
 'use strict';
 /* jshint undef:false */
+/*
+ * code font: https://github.com/esvit/ng-ckeditor/blob/master/ng-ckeditor.js
+ */
 
+var $defer, loaded;
 angular.module('cmsApp')
-  .directive('ckEditor', function (ENV) {
+  .run(['$q', '$timeout', function($q, $timeout) {
+    $defer = $q.defer();
+
+    if (angular.isUndefined(window.CKEDITOR)) {
+      throw new Error('CKEDITOR not found');
+    }
+    CKEDITOR.disableAutoInline = true;
+    function checkLoaded() {
+      if (CKEDITOR.status === 'loaded') {
+        loaded = true;
+        $defer.resolve();
+      } else {
+        checkLoaded();
+
+      }
+    }
+    CKEDITOR.on('loaded', checkLoaded);
+    $timeout(checkLoaded, 100);
+  }])
+  .directive('ckEditor', function ($q, $timeout, ENV) {
     return {
       require : '?ngModel',
       link : function($scope, element, attr, ngModel) {
-        var ck = window.CKEDITOR.replace(element[0]);
+        var form    = null;
+        var EMPTY_HTML = '<p></p>',
+        data = [],
+        isReady = false;
 
-        CKEDITOR.plugins.addExternal('youtube', ENV.basepath+'/ckeditor-plugins/youtube/', 'plugin.js');
+        var onLoad = function () {
+          var options = {
+            toolbar: 'full',
+            /*jshint camelcase: false */
+            toolbar_full: [
+              { name: 'basicstyles',
+                items: [ 'Bold', 'Italic', 'Strike', 'Underline' ] },
+                { name: 'paragraph', items: [ 'BulletedList', 'NumberedList', 'Blockquote' ] },
+                { name: 'editing', items: ['JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+                { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ]},
+                { name: 'tools', items: [ 'SpellChecker', 'Maximize' ] },
+                '/',
+                { name: 'styles', items: [ 'Format', 'FontSize', 'TextColor', 'PasteText', 'PasteFromWord', 'RemoveFormat' ] },
+                { name: 'insert', items: [ 'Image', 'Youtube', 'Table', 'SpecialChar' ]},
+                { name: 'forms', items: [ 'Outdent', 'Indent' ] },
+                { name: 'clipboard', items: [ 'Undo', 'Redo' ] },
+                { name: 'document', items: [ 'PageBreak', 'Source' ] }
+            ],
+            disableNativeSpellChecker: false,
+            uiColor: '#FAFAFA',
+            height: '400px',
+            width: '100%'
+          };
 
-        ck.config.extraPlugins = 'youtube,justify,image2';
-        ck.config.language = 'pt-BR';
-        ck.config.height = '100%';
+          var instance = CKEDITOR.replace(element[0], options);
+          CKEDITOR.plugins.addExternal('youtube', ENV.basepath+'/ckeditor-plugins/youtube/', 'plugin.js');
+          instance.config.extraPlugins = 'youtube,justify,image2';
+          instance.config.language = 'pt-BR';
+          var configLoaderDef = $q.defer();
 
-        if(!ngModel) {
-          return;
-        }
-
-        ck.on('maximize', function() {
-          window.scrollTo(0, 0);
-        });
-
-        ck.on('pasteState', function() {
-          $scope.$apply(function() {
-            ngModel.$setViewValue(ck.getData());
+          element.bind('$destroy', function () {
+            instance.destroy(
+              false //If the instance is replacing a DOM element, this parameter indicates whether or not to update the element with the instance contents.
+            );
           });
-        });
+          var setModelData = function(setPristine) {
+            var data = instance.getData();
+            if (data === '') {
+              data = null;
+            }
+            $timeout(function () { // for key up event
+              if(setPristine !== true || data !== ngModel.$viewValue) {
+                ngModel.$setViewValue(data);
+              }
+              if(setPristine === true && form){
+                form.$setPristine();
+              }
+            }, 0);
+          }, onUpdateModelData = function(setPristine) {
+            if (!data.length) { return; }
 
-        ngModel.$render = function() {
-          ck.setData(ngModel.$viewValue);
+
+            var item = data.pop() || EMPTY_HTML;
+            isReady = false;
+            instance.setData(item, function () {
+              setModelData(setPristine);
+              isReady = true;
+            });
+          };
+
+          instance.on('pasteState',   setModelData);
+          instance.on('change',       setModelData);
+          instance.on('blur',         setModelData);
+          //instance.on('key',          setModelData); // for source view
+
+          instance.on('instanceReady', function() {
+            $scope.$broadcast('ckeditor.ready');
+            $scope.$apply(function() {
+              onUpdateModelData(true);
+            });
+
+            instance.document.on('keyup', setModelData);
+          });
+          instance.on('customConfigLoaded', function() {
+            configLoaderDef.resolve();
+          });
+
+          ngModel.$render = function() {
+            data.push(ngModel.$viewValue);
+            if (isReady) {
+              onUpdateModelData();
+            }
+          };
         };
+
+        if (CKEDITOR.status === 'loaded') {
+          loaded = true;
+        }
+        if (loaded) {
+          onLoad();
+        } else {
+          $defer.promise.then(onLoad);
+        }
       }
-    };
-  });
+      };
+    });
