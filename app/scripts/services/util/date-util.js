41d910316cda3a8bc1fad0d272f4e2584630df73
RatoX <rodrigovieira18@gmail.com>
2014-09-20 17:44:36 -0300

    Funcao para obter a data em ISO

--- app/scripts/services/util/date-util.js
+++ app/scripts/services/util/date-util.js
@@ -9,6 +9,24 @@
  */
 angular.module('cmsApp')
   .service('DateUtil', function DateUtil($filter) {
+    function pad(num) {
+      var norm = Math.abs(Math.floor(num));
+      return (norm < 10 ? '0' : '') + norm;
+    }
+
+    function toISO8601(local) {
+      var tzo = -local.getTimezoneOffset();
+      var sign = tzo >= 0 ? '+' : '-';
+      return local.getFullYear() +
+        '-' + pad(local.getMonth()+1) +
+        '-' + pad(local.getDate()) +
+        'T' + pad(local.getHours()) +
+        ':' + pad(local.getMinutes()) +
+        ':' + pad(local.getSeconds()) +
+        sign + pad(tzo / 60) +
+        ':' + pad(tzo % 60);
+    }
+
     this.now = {
       getMonth: function (){
         return (new Date()).getMonth();
@@ -22,6 +40,10 @@ angular.module('cmsApp')
       var date = new Date(year, month);
 
       return $filter('date')(date,'yyyy/MM');
+    };
+
+    this.toISO8601 = function(date){
+      return toISO8601(new Date(date));
     };
 
     this.fromISO8601 = function(dateISO){
