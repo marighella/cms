b7f41ec95cb10408e85f4a764e264634005339a3
RatoX <rodrigovieira18+raphael+rodrigo@gmail.com>
2014-09-30 15:26:14 -0300

    #4

--- test/spec/directives/dynamic-required.js
+++ test/spec/directives/dynamic-required.js
@@ -12,39 +12,42 @@ describe('Directive: dynamicRequired', function () {
     scope = $rootScope.$new();
   }));
 
-  it('should put default required when do not have field "need"', inject(function ($compile) {
+  describe('#need', function() {
+    it('should put default required when do not have field "need"', inject(function ($compile) {
 
-    scope.field = {
-    
-    };
-    element = angular.element('<input dynamic-required="field"/>');
-    element = $compile(element)(scope);
-    expect(element.attr('ng-required')).toBe('field.required');
-  }));
+      scope.field = {
+      
+      };
+      element = angular.element('<input dynamic-required="field"/>');
+      element = $compile(element)(scope);
+      expect(element.attr('ng-required')).toBe('field.required');
+    }));
 
-  it('should put need expression', inject(function ($compile) {
-    scope.field = {
-      need: {
-        field: 'section',
-        equal: true,
-        value: 'tv'
-      }
-    };
-    element = angular.element('<input dynamic-required="field"/>');
-    element = $compile(element)(scope);
-    expect(element.attr('ng-required')).toBe('entity.section===\'tv\'');
-  }));
+    it('should put need expression', inject(function ($compile) {
+      scope.field = {
+        need: {
+          field: 'section',
+          equal: true,
+          value: 'tv'
+        }
+      };
+      element = angular.element('<input dynamic-required="field"/>');
+      element = $compile(element)(scope);
+      expect(element.attr('ng-required')).toBe('entity.section===\'tv\'');
+    }));
 
-  it('should put need expression ', inject(function ($compile) {
-    scope.field = {
-      need: {
-        field: 'section',
-        equal: false,
-        value: 'tv'
-      }
-    };
-    element = angular.element('<input dynamic-required="field"/>');
-    element = $compile(element)(scope);
-    expect(element.attr('ng-required')).toBe('entity.section!==\'tv\'');
-  }));
+    it('should put need expression ', inject(function ($compile) {
+      scope.field = {
+        need: {
+          field: 'section',
+          equal: false,
+          value: 'tv'
+        }
+      };
+      element = angular.element('<input dynamic-required="field"/>');
+      element = $compile(element)(scope);
+      expect(element.attr('ng-required')).toBe('entity.section!==\'tv\'');
+    }));
+  });
+
 });
