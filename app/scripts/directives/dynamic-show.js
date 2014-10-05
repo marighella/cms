363846fbbedb04e4273f2ff0294f842910fd6fee
willmendesneto <willmendesneto@gmail.com>
2014-10-05 13:48:05 -0300

    Add scope in dynamicShow for remove $scope.$eval() WIP #3

--- app/scripts/directives/dynamic-show.js
+++ app/scripts/directives/dynamic-show.js
@@ -10,26 +10,31 @@ angular.module('cmsApp')
   .directive('dynamicShow', function ($compile) {
     return {
       restrict:'A',
-      terminal:true,
-      priority:1000,
-      link: function postLink(scope, element, attrs) {
-        var field = scope.$eval(attrs.dynamicShow);
-        if(!field){
-          return;
+      scope: {
+        field: '=field'
+      },
+      link: {
+        post: function (scope, element, attrs) {
+
+          var field = scope.field || false;
+          if(!field){
+            return;
+          }
+
+          var need = field.need || false;
+
+          if(need){
+             var requiredField = need.field;
+             var requiredValue = need.value;
+             var condition     = (need.equal) ? '===' : '!==';
+             var query = ['entity.',requiredField,condition,'\'',requiredValue,'\''].join('');
+
+             element.attr('ng-show', query);
+          }
+
+          element.removeAttr('dynamic-show');
+          $compile(element)(scope);
         }
-        var need = field.need;
-
-        if(need){
-           var requiredField = need.field;
-           var requiredValue = need.value;
-           var condition     = (need.equal) ? '===' : '!==';
-           var query = ['entity.',requiredField,condition,'\'',requiredValue,'\''].join('');
-
-           element.attr('ng-show', query);
-        }
-
-        element.removeAttr('dynamic-show');
-        $compile(element)(scope);
       }
     };
   });
