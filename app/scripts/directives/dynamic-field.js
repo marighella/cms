69780ea1dfcabd8f8f8165f648a022649086ceea
nicolastrres <nicolastrres@gmail.com>
2014-10-23 11:31:01 -0300

    Para a sessão de 'Destaque' não é obrigatório a imagem de capa

--- app/scripts/directives/dynamic-field.js
+++ app/scripts/directives/dynamic-field.js
@@ -15,16 +15,26 @@ angular.module('cmsApp')
 
     function addDynamicRequired(element, field){
       var need = field.need;
+      var queries = [];
+      var values =[];
 
       if(need){
         var requiredField = need.field;
-        var requiredValue = need.value;
-        var condition     = (need.equal) ? '===' : '!==';
+        if(typeof need.value === "string")
+          values.push(need.value);    
+        else 
+          values = need.value;
 
-
-        var query = ['entity.',requiredField,condition,'\'',requiredValue,'\''].join('');
+        for(var i=0; i < values.length; i++){
+          var requiredValue = values[i];
+          var condition     = (need.equal) ? '===' : '!==';
+          queries[i] = ['entity.',requiredField,condition,'\'',requiredValue,'\''].join('');
+        }
+       
+        var query = queries.join('&&');
         element.attr('ng-required', query);
         element.attr('ng-show', query);
+      
       }else {
         element.attr('ng-required', 'field.required');
       }
