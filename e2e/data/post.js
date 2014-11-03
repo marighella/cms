2bf440a39f8c407e5d0dca08cf5bbfac30b67225
RatoX <rodrigovieira18+raphael+rodrigo@gmail.com>
2014-11-03 11:56:02 -0300

    Tentativa de ajustes de pasta

---
+++ e2e/data/post.js
@@ -0,0 +1,19 @@
+var Post = function (){
+    var date =  new Date();
+    var month = date.getMonth() + 1;
+    var time = date.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+    var TIMESTAMP = date.getDate()+"/"+month+"/"+date.getFullYear()+" "+time;
+
+    this.FILETOUPLOAD = "/Users/nicolastrres/Documents/mst_capa.png";
+    this.DATE = "01/01/2010";
+    this.TIME = "1:01 PM";
+    this.HAT = "Teste chapeu";
+    this.TITLE = "Teste titulo "+TIMESTAMP;
+    this.SUPPORTLINE = "Teste linha fina";
+    this.MENU = "meio ambiente";
+    this.SECTION = "---Nenhum---";
+    this.SECTIONVIDEO = "Vídeo";
+    this.TAG = "Teste artigo normal";
+    this.VIDEO = "https://www.youtube.com/watch?v=QixID6N6ImM";
+}
+module.exports = new Post();
