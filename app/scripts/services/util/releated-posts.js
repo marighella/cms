07bec77c64848da45a3f229669013a2645deb7c2
RatoX <rodrigovieira18@gmail.com>
2014-12-22 21:45:01 -0300

    WIP

---
+++ app/scripts/services/util/releated-posts.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name cmsApp.ReleatedPosts
+ * @description
+ * # Dateutil
+ * Service in the cmsApp.
+ */
+angular.module('cmsApp')
+  .service('ReleatedPosts', function PostUtil(Repository, _) {
+    var intersection = function(array){
+      if (!array) {
+        return [];
+      }
+
+      var result = [];
+      var argsLength = arguments.length;
+      var args = arguments;
+      var header = 0;
+      var finished = false;
+
+      while(!finished){
+        for( var i = header; i < argsLength; i++){
+          if( i !== header ){
+            var inter = _.intersection(args[header], args[i]);
+            result.push(inter);
+          }
+        }
+        
+        header++;
+        finished = (header === argsLength);
+      }
+
+      console.log(result);
+      var agroups = _.flatten(_.compact(result));
+    
+
+      return _.countBy(agroups);
+    };
+
+    this.getPostsByTags = function(tags){
+      var tagsJson = Repository.tagsFile;
+      var allPosts = _.values(_.pick(tagsJson, tags));
+
+
+      var result = intersection.apply(this, allPosts);
+      
+
+      return result;
+    };
+  });
---
+++ test/spec/services/util/releated-posts.js
@@ -0,0 +1,38 @@
+'use strict';
+/*jshint camelcase: false */
+
+describe('Service: ReleatedPosts', function () {
+
+  // load the service's module
+  beforeEach(module('cmsApp'));
+
+  // instantiate service
+  var ReleatedPosts;
+  beforeEach(inject(function (_ReleatedPosts_) {
+    ReleatedPosts = _ReleatedPosts_;
+  }));
+
+  it('should do something', function () {
+    expect(!!ReleatedPosts).toBe(true);
+  });
+
+  describe('give a tag-posts file and a set of tags', function (){
+    beforeEach(inject(function (_Repository_) {
+        _Repository_.tagsFile = {'Banana':['dois','tres', 'sete'],
+                                 'Cianureto':['tres','sete'],
+                                 'Manga':['dois','quatro'],
+                                 'Maca':['quatro', 'seis', 'sete']};
+
+      })
+    );
+    it('should get the posts with more relevance to a set of tags', function(){
+      var tags = ['Banana','Maca'];
+      expect(ReleatedPosts.getPostsByTags(tags)).toEqual({'sete':2});
+    });
+
+    it('should get the posts with more relevance to a set of tags', function(){
+      var tags = ['Banana','Maca','Cianureto'];
+      expect(ReleatedPosts.getPostsByTags(tags)).toEqual({'sete':3,'tres':2});
+    });
+  });
+});
