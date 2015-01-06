e50d9f48f1dee5ef671f8e699485aaf1a378b481
Gabriel Caires <gabrielcairesmar@gmail.com>
2015-01-06 17:13:12 -0300

    removendo imagemim

--- Gruntfile.js
+++ Gruntfile.js
@@ -285,16 +285,16 @@ module.exports = function (grunt) {
     //   dist: {}
     // },
 
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
-          dest: '<%= yeoman.dist %>/images'
-        }]
-      }
-    },
+    // imagemin: {
+    //   dist: {
+    //     files: [{
+    //       expand: true,
+    //       cwd: '<%= yeoman.app %>/images',
+    //       src: '{,*/}*.{png,jpg,jpeg,gif}',
+    //       dest: '<%= yeoman.dist %>/images'
+    //     }]
+    //   }
+    // },
 
     svgmin: {
       dist: {
@@ -404,7 +404,7 @@ module.exports = function (grunt) {
       dist: [
         'copy:styles',
         'htmlmin',
-        'imagemin',
+        // 'imagemin',
         'svgmin'
       ]
     },
