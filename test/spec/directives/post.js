b0076596291d3f14577c97760e2baeb02f9d1ed3
Rafael Oliveira <ludug3r0@gmail.com>
2015-02-05 15:47:52 -0300

    #39 Teste cobrindo highlight de tags

---
+++ test/spec/directives/post.js
@@ -0,0 +1,68 @@
+'use strict';
+
+describe('Directive: post', function () {
+  // load the directive's module
+  beforeEach(module('cmsApp'));
+  beforeEach(module('templates'));
+
+  var element, scope;
+
+  beforeEach(inject(function ($rootScope, $q, Repository) {
+    var deferred = $q.defer();
+    Repository.content.get = function() {
+      deferred.resolve({metadata: {tags: [{tag: 'saude'}, {tag: 'juventude'}]}});
+      return deferred.promise;
+    };
+    scope = $rootScope.$new();
+  }));
+
+  it('should list tags in common with related post', inject(function($compile, _) {
+    scope.related = {
+      link: 'related post url'
+    };
+   
+    element = angular.element('<post post="releated" tags="tags"></post>');
+    element = $compile(element)(scope);
+
+    scope.$digest();
+
+    var tagElements = _.chain(element.find('span'))
+    .map(function(elem){
+       return angular.element(elem);
+    })
+    .filter(function (elem) {
+       return elem.hasClass('tag');
+    })
+    .value();
+
+    expect(tagElements.length).toBe(2);
+    expect(tagElements[0].text()).toContain('saude');
+    expect(tagElements[1].text()).toContain('juventude');
+  }));
+
+
+  it('should highlight tags in common on related posts', inject(function ($compile, _) {
+    scope.releated = {
+      link: 'related post url'
+    };
+    scope.tags = [{tag: 'saude'}, 
+                  {tag: 'encontro'}];
+    
+    element = angular.element('<post post="releated" tags="tags"></post>');
+    element = $compile(element)(scope);
+
+    scope.$digest();
+
+    var tagElements = _.chain(element.find('span'))
+    .map(function(elem){
+       return angular.element(elem);
+    })
+    .filter(function (elem) {
+       return elem.hasClass('tag') && elem.hasClass('selected');
+    })
+    .value();
+
+    expect(tagElements.length).toBe(1);
+    expect(tagElements[0].text()).toContain('saude');
+  }));
+});
